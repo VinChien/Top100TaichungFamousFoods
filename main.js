4d09fe74ed8312f4782b27c736bc24671d984c22
jianfan0114@gmail.com <jianfan0114@gmail.com>
2020-08-04 20:28:57 +0800

    test

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 new Vue({
-    el: '#info',
+    el: '#main',
     data: {
-        datas: null
+        datas: null,
+        areas: [],
+        visibility: 'all',
     },
-    mounted() {
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://datacenter.taichung.gov.tw/swagger/OpenData/4d40ce70-21d1-48e7-ba7c-5a9257faf076");
-        xhr.send();
-
-        xhr.onload = () => {
-            let json = JSON.parse(xhr.responseText);
-            this.datas = json;
+    mounted: function () {
+        this.$nextTick(function () {
+            let xhr = new XMLHttpRequest();
+            xhr.open("GET", "https://datacenter.taichung.gov.tw/swagger/OpenData/4d40ce70-21d1-48e7-ba7c-5a9257faf076");
+            xhr.send();
+            xhr.onload = () => {
+                let json = JSON.parse(xhr.responseText);
+                this.datas = json;
+            };
+        })
+    },
+    computed: {
+        datasArea: function () {
+            let vm = this.areas;
+            this.datas.forEach(el => {
+                vm.push(el['區域']);
+            });
+            this.areas = Array.from(new Set(vm));
         }
     },
 })
